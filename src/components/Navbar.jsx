502f4d5365087f6dd4720c04435309d82223532c
EatSleepRunTimeErrorRepeat <ryan.letchman@mail.kmutt.ac.th>
2023-03-25 23:40:01 +0700

    Initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import "./navbar.css";
+function Navbar(){
+    return(
+        <div>
+            <h1 className="task">Tasks</h1>
+            <nav>
+                <li>Incoming</li>
+                <li>Today</li>
+                <li>Calendar</li>
+            </nav>
+        </div>
+    )
+}
+export default Navbar;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,12 @@
+import "./Profile.css"
+function Profile (){
+    return(
+        <div className="layout">
+            <img src="assets/ryan.jpg" alt="profile"/>
+            <h1 className="name">Ryan Letchman</h1>
+            <hr></hr>
+        </div>
+    )
+}
+
+export default Profile;
