8998fe62717195256df99226af2200e230eb50da
EatSleepRunTimeErrorRepeat <ryan.letchman@mail.kmutt.ac.th>
2023-04-07 15:18:53 +0700

    Messed up the file

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import "./App.css";
 import Home from "./components/Home";
 import Todo from "./components/Todo";
+import TodoPage from "./pages/Todopage";
+import Calendar from "./pages/Calendar";
+import { Routes, Route } from "react-router-dom";
+
 function App() {
     return (
-        <div className="App">
-            <div className="home">
-                <Home />
-            </div>
-            <div className="todo">
-                <Todo />
-            </div>
-        </div>
+        <Routes>
+            <Route exect path="/" element={<Home />} />
+            <Routh path="/todo" element={<Todo />} />
+            <Routh path="/today" element={<TodoPage />} />
+            <Routh path="/calendar" element={<Calendar />} />
+        </Routes>
     );
 }
 export default App;
